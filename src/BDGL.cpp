ffa1fb49f6a54380441650bf524b22ac9a922f01
Bennet D <bennet.deboben@gmail.com>
2022-08-18 00:29:47 +0200

    add vertex color to triangle

--- src/BDGL.cpp
+++ src/BDGL.cpp
@@ -1,18 +1,23 @@
 #include "BDGL.h"
 #include "shader.h"
+#include <glm/common.hpp>
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;"
+    "out vec3 ourColor;"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   ourColor = aColor;"
     "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "in vec3 ourColor;"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vec4(ourColor, 1.0);\n"
     "}\n\0";
 
 unsigned int shaderProgram = 0;
@@ -31,13 +36,13 @@ void BDGL::createWindow(std::string name) {
     window = glfwCreateWindow(800,800, name.c_str(), NULL, NULL);
     if(window == NULL) {
         glfwTerminate();
-        throw std::exception("Failed to create GLFW window");
+        throw std::runtime_error("Failed to create GLFW window");
     }
     glfwMakeContextCurrent(window);
 
     //Init GLAD
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        throw std::exception("Failed to init GLAD");
+        throw std::runtime_error("Failed to init GLAD");
     }
 
     glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) {
@@ -51,33 +56,39 @@ void BDGL::init() {
 
     // VERTICES
     float vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
-    };
+        // positions         // colors
+        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+        0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+    };    
     unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
+        0, 1, 2   // first triangle
+        //1, 2, 3    // second triangle
     }; 
 
+    // gen buffers
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);  
     glGenBuffers(1, &EBO);
 
+    // bind objects
     glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); 
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);  
+    // copy data to gpu
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // configure and enable attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);  
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0); 
-    glBindVertexArray(0); 
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    //glBindVertexArray(0); 
 }
 
 void BDGL::run() {
@@ -88,7 +99,11 @@ void BDGL::run() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
         glUseProgram(shaderProgram);
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
@@ -96,6 +111,7 @@ void BDGL::run() {
         glfwPollEvents();
     }
 
+    // optional
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
