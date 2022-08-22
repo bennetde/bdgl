12fbd09fbf503d746098bf1caba17215edf8efb1
Bennet D <bennet.deboben@gmail.com>
2022-08-23 00:36:32 +0200

    fix fragment shaders

--- resources/shaders/f_pink.glsl
+++ resources/shaders/f_pink.glsl
@@ -3,4 +3,4 @@ out vec4 FragColor;
 void main()
 {
    FragColor = vec4(1.0,0.0,1.0, 1.0);
-};
+}
--- resources/shaders/fragment.glsl
+++ resources/shaders/fragment.glsl
@@ -1,4 +1,4 @@
-#version 410 core
+#version 330 core
 out vec4 FragColor;
 in vec2 texCoord;
 
@@ -9,5 +9,5 @@ uniform sampler2D texture2;
 void main()
 {
    //FragColor = vec4(ourColor, 1.0);
-   FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), .5); /** vec4(ourColor, 1.0)*/
-};
+   FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), .5);
+}
